46fb139df215d04e132b22ac4c672a3a10fd90b4
shawnfc <shawn@shawnconrad.com>
2016-10-07 18:43:17 -0400

    Shawn Conrad's Angular Starter

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+// Root Component
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Angular 2 Ready to Go!</h1>`
+})
+
+export class AppComponent {}
